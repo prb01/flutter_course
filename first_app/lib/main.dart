472b631eb029a062d256dc3c1d4e09299164fa4a
prb01 <pbergstroem@academyofmine.com>
2024-08-02 09:00:56 +0200

    first push

---
+++ first_app/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlueAccent,
+        body: Center(
+          child: Text(
+            "Nabu",
+            style: TextStyle(
+              color: Colors.black45,
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
